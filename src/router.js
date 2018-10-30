2dde8991e74b5c72e1057edc7c1e8b52cf3258bf
soner10 <0897806@hr.nl>
2018-10-30 13:44:12 +0100

    Login pagina
    login pagina aangemaakt

--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Pagina1 from './pagina1';
-import Pagina2 from './pagina2';
+import Login from './login';
+
+
 
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={Pagina1} />
-    <Route path="/pagina2" component={Pagina2} />
+    <Route path="/login" component={Login} />
     <Route path="/pagina1" component={Pagina1} />
   </Switch>
 )
