e811015a07d69c298999f55c458364045afd605c
Scizivire <danilolima@live.nl>
2018-11-14 16:49:48 +0100

    contact final

--- src/contact.js
+++ src/contact.js
@@ -6,37 +6,34 @@ class Contact extends Component {
     render() {
 
         return (
-            <div class='container contact-container'>
-                <div class='row'>
-                    <div class="col-md-6 contact-form">
-                        <h1>Contact us:</h1>
-                        <h3>If you have any feedback or questions, feel free to fill in the form below:</h3>
+            <div className='container contact-container'>
+                <div className='row'>
+                    <div className="col-md-6 contact-form">
+                        <h1>Contact us</h1>
+                        <h4>If you have any feedback or questions, feel free to fill in the form below:</h4>
                         <form>
-                            <h3>Your name (optional):</h3>
-                            <div class="form-group">
-                                <input type='text' class="form-control" placeholder="Name" value="" />
+                            <h5>Your name (optional):</h5>
+                            <div className="form-group">
+                                <input type='text' className="form-control"/>
                             </div>
-                            <h3>Your e-mail address:</h3>
-                            <div class="form-group">
-                                <input type='text' class="form-control" value="" />
+                            <h5>Your company (optional):</h5>
+                            <div className="form-group">
+                                <input type='text' className="form-control"/>
                             </div>
-                            <h3>Your company (optional):</h3>
-                            <div class="form-group">
-                                <input type='text' class="form-control" value="" />
+                            <h5>Your e-mail address:</h5>
+                            <div className="form-group">
+                                <input type='text' className="form-control"/>
                             </div>
-                            <h3>Brief description of your question:</h3>
-                            <div class="form-group">
-                                <input type='text' class="form-control" placeholder="Max 100 characters" value="" />
+                            <h5>Brief description of your question:</h5>
+                            <div className="form-group">
+                                <input type='text' className="form-control" placeholder="Max 100 characters"/>
                             </div>
-                            <h3>Your question:</h3>
-                            <div class="form-group">
-                                <input type='text'class="form-control" placeholder="Max 1000 characters" value="" />
+                            <h5>Your question:</h5>
+                            <div className="form-group">
+                                <input type='text' className="form-control" placeholder="Max 1000 characters"/>
                             </div>
-                            <div class="form-group">
-                                <a href="#" class="AccountMade">Do you already have an account?</a>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" class="btnSubmit" value="Send" />
+                            <div className="form-group">
+                                <input type="submit" className="btnSubmit" value="Send" />
                             </div>
                         </form>
                     </div>
