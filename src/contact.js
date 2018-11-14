d16e3f0063d953e508346f7d6ee20f4ab1d08baa
soner10 <0897806@hr.nl>
2018-11-14 22:08:35 +0100

    Contactpagina verandert
    Contactpagina verandert. Andere uiterlijk en indeling gegeven. Oude versie zag er niet zo netjes uit.

--- src/contact.js
+++ src/contact.js
@@ -6,39 +6,50 @@ class Contact extends Component {
     render() {
 
         return (
-            <div className='container contact-container'>
-                <div className='row'>
-                    <div className="col-md-6 contact-form">
-                        <h1>Contact us</h1>
-                        <h4>If you have any feedback or questions, feel free to fill in the form below:</h4>
+          <div class='container'>
+            <div class='row'>
+                <div class='col-sm-10 col-sm-offset-1'>
+                    <div class='well'>
                         <form>
-                            <h5>Your name (optional):</h5>
-                            <div className="form-group">
-                                <input type='text' className="form-control"/>
-                            </div>
-                            <h5>Your company (optional):</h5>
-                            <div className="form-group">
-                                <input type='text' className="form-control"/>
-                            </div>
-                            <h5>Your e-mail address:</h5>
-                            <div className="form-group">
-                                <input type='text' className="form-control"/>
-                            </div>
-                            <h5>Brief description of your question:</h5>
-                            <div className="form-group">
-                                <input type='text' className="form-control" placeholder="Max 100 characters"/>
-                            </div>
-                            <h5>Your question:</h5>
-                            <div className="form-group">
-                                <input type='text' className="form-control" placeholder="Max 1000 characters"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="submit" className="btnSubmit" value="Send" />
+                            <div class='row'>
+                                <div class='col-sm-4'>
+                                    <div class='form-group'>
+                                        <label for='fname'>First Name</label>
+                                        <input type='text' name='fname' class='form-control' placeholder="Heb het aangepast;) Gr, Soner."/>
+                                    </div>
+                                    <div class='form-group'>
+                                        <label for='lname'>Last Name</label>
+                                        <input type='text' name='lname' class='form-control' />
+                                    </div>
+                                    <div class='form-group'>
+                                        <label for='email'>Email</label>
+                                        <input type='email' name='email' class='form-control' />
+                                    </div>
+                                    <div class='form-group'>
+                                        <label for='subject'>Subject</label>
+                                        <select name='subject' class='form-control'>
+                                            <option>General Inquiry</option>
+                                            <option>Site Suggestions</option>
+                                            <option>Product Support</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class='col-sm-8'>
+                                    <div class='form-group'>
+                                        <label for='message'>Message</label>
+                                        <textarea class='form-control' name='message' rows='10'></textarea>
+                                    </div>
+                                    <div class='text-right'>
+                                        <input type='submit' class='btn btn-primary' value='Submit' />
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
+          </div>
+
         );
     }
 }
