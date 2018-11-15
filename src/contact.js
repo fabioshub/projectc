d970302aa62db7afb0b1e242b0821a30db5bd115
Scizivire <danilolima@live.nl>
2018-11-15 12:11:51 +0100

    contact fix
    die layout was een beetje verkloot

--- src/contact.js
+++ src/contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
     render() {
 
         return (
-          <div class='container'>
+          <div class='contact-container'>
             <div class='row'>
                 <div class='col-sm-10 col-sm-offset-1'>
                     <div class='well'>
